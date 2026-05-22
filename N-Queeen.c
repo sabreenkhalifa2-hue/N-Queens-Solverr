95fb63761e477c17c21bb8bfcb7f2a0cdb22bc8f
sabreenkhalifa2-hue <sabreenkhalifa2@gmail.com>
2026-05-22 15:28:10 +0200

    tt

--- N-Queeen.c
+++ N-Queeen.c
@@ -51,3 +51,80 @@ private:
 
         return maxPairs - conflicts;
     }
+
+    Individual createRandom() {
+        Individual ind;
+
+        for (int i = 0; i < N; i++) {
+            ind.rows[i] = 1 + myRandom(N);
+        }
+
+        ind.score = calculateScore(ind.rows);
+
+        return ind;
+    }
+
+  
+    Individual rouletteSelection(Individual pop[max_solutions]) {
+
+        int total = 0;
+
+        for (int i = 0; i < max_solutions; i++) {
+            total += pop[i].score;
+        }
+
+        if (total == 0) {
+            return pop[myRandom(max_solutions)];
+        }
+
+        int point = myRandom(total + 1);
+
+        int cumulative = 0;
+
+        for (int i = 0; i < max_solutions; i++) {
+
+            cumulative += pop[i].score;
+
+            if (cumulative >= point) {
+                return pop[i];
+            }
+        }
+
+        return pop[max_solutions - 1];
+    }
+
+    Individual tournamentSelection(Individual pop[max_solutions]) {
+
+        Individual best = pop[myRandom(max_solutions)];
+
+        for (int i = 1; i < sample; i++) {
+
+            Individual candidate = pop[myRandom(max_solutions)];
+
+            if (candidate.score > best.score) {
+                best = candidate;
+            }
+        }
+
+        return best;
+    }
+
+    Individual crossover(Individual p1, Individual p2) {
+
+        Individual child;
+
+        int cutPoint = 1 + myRandom(N - 1);
+
+        for (int i = 0; i < cutPoint; i++) {
+            child.rows[i] = p1.rows[i];
+        }
+
+        for (int i = cutPoint; i < N; i++) {
+            child.rows[i] = p2.rows[i];
+        }
+
+        child.score = calculateScore(child.rows);
+
+        return child;
+    }
+
